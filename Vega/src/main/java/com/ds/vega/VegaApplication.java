e4f5b8cead274ddcac152b72f911a26b8c3a0f8c
msoloviom <marinaby@i.ua>
2018-03-25 01:20:34 +0200

    create service skeleton

---
+++ Vega/src/main/java/com/ds/vega/VegaApplication.java
@@ -0,0 +1,12 @@
+package com.ds.vega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VegaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VegaApplication.class, args);
+	}
+}
---
+++ Vega/src/test/java/com/ds/vega/VegaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ds.vega;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VegaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
